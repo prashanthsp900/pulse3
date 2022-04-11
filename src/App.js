5af3add692a779f90c24267b998abaa437f0f809
prashanthsp900 <101685774+prashanthsp900@users.noreply.github.com>
2022-04-11 06:38:51 +0530

    added new heading

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <h1>RRR is the highest grossing cinema this week</h1>
       <h1>The Kashmiri Files is the second highest grossing movie this week</h1>
+    <h1>The next highest grossing movie would be Avatar II</h1>
     </div>
   );
 }
